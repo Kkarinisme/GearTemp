039e54346f0945a5d1a150810a6accfc3e1096cf
Karin Olson <Kkarinisme@aol.com>
2013-05-21 09:45:12 -0400

    Hopefully adding armor.cs to project

---
+++ GearFoundry/Inventory/armor.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Linq;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Windows.Forms;
+using System.Threading;
+using WindowsTimer = System.Windows.Forms.Timer;
+using Decal.Adapter;
+using Decal.Filters;
+using Decal.Adapter.Wrappers;
+using System.Xml.Linq;
+using System.IO;
+
+namespace GearFoundry
+{
+    public partial class PluginCore : PluginBase
+    {
+        XDocument xdocArmor;
+        WindowsTimer mWaitingForArmorIDTimer = new WindowsTimer();
+
+
+        void btnGetToonArmor_Click(object sender, MyClasses.MetaViewWrappers.MVControlEventArgs e)
+        {
+            doGetArmor();
+        }
+
+        private void doGetArmor()
+        {
+            try
+            {
+                WriteToChat("ToonArmorButton was clicked");
+                mWaitingForArmorID = new List<WorldObject>();
+
+                armorFilename = toonDir + @"\" + toonName + "Armor.xml";
+                genArmorFilename = currDir + @"\allToonsArmor.xml";
+                holdingArmorFilename = world + @"\holdingArmor.xml";
+                
+
+                xdocArmor = new XDocument(new XElement("Objs"));
+
+
+                if (!File.Exists(genArmorFilename))
+                {
+
+                    XDocument tempDoc = new XDocument(new XElement("Objs"));
+                    tempDoc.Save(genArmorFilename);
+                    tempDoc = null;
+
+
+                }
+
+
+
+                foreach (Decal.Adapter.Wrappers.WorldObject armorobj in Core.WorldFilter.GetInventory())
+                {
+                    try
+                    {
+
+                         if (armorobj.Values(LongValueKey.Slot) == -1)
+                        {
+
+                            Globals.Host.Actions.RequestId(armorobj.Id);
+                            mWaitingForArmorID.Add(armorobj);
+                        }
+                    }
+                    catch (Exception ex) { LogError(ex); }
+
+                } // endof foreach world object
+                // initialize event timer for processing inventory
+                mWaitingForArmorIDTimer.Tick += new EventHandler(ArmorTimerEventProcessor);
+
+                // Sets the timer interval to 5 seconds.
+                mWaitingForArmorIDTimer.Interval = 10000;
+
+
+                ProcessArmorDataInventory();
+                mArmorIsFinished();
+            }
+            catch (Exception ex) { LogError(ex); }
+        }
+
+        private void mArmorIsFinished()
+        {
+            try
+            {
+                int na = mWaitingForArmorID.Count;
+                     if (mWaitingForArmorID.Count > 0)
+                    {
+
+                        mArmorCountWait();
+                    }
+
+                    else if (mWaitingForArmorID.Count == 0)
+                    {
+
+                        try
+                        {
+                            xdocArmor.Save(armorFilename);
+                            XDocument xdocGenArmor = XDocument.Load(genArmorFilename);
+                            xdocGenArmor.Descendants("Obj").Where(x => x.Element("ToonName").Value == toonName).Remove();
+
+                            xdocGenArmor.Root.Add(XDocument.Load(armorFilename).Root.Elements());
+
+                            xdocGenArmor.Save(genArmorFilename);
+                            GearFoundry.PluginCore.WriteToChat("General Armor file has been saved. ");
+
+                        }
+                        catch (Exception ex) { LogError(ex); }
+
+                    }
+
+                    m = 30;
+                    k = 0;
+                    n = 0;
+                    mWaitingForID = null;
+                    xdoc = null;
+                    fn = null;
+
+                
+            }
+
+            catch (Exception ex) { LogError(ex); }
+        }
+
+        private void mArmorCountWait()
+        {
+            mWaitingForArmorIDTimer.Start();
+        }
+ 
+
+        public void ArmorTimerEventProcessor(Object Sender, EventArgs mWaitingForArmorIDTimer_Tick)
+        {
+            try
+            {
+                mWaitingForArmorIDTimer.Stop();
+                for (int n = 0; n < mWaitingForArmorID.Count; n++)
+                {
+                    if (mWaitingForArmorID[n].HasIdData)
+                    {
+                        ProcessArmorDataInventory();
+                        mArmorIsFinished();
+
+                    }
+                    else
+                    {
+                        mArmorCountWait(); }
+                }
+
+            }
+            catch (Exception ex) { LogError(ex); }
+
+        }
+
+ 
+        public void removeToonfromArmorFile()
+        {
+            try
+            {
+
+                xdocArmor = XDocument.Load(genArmorFilename);
+
+                IEnumerable<XElement> myelements = xdocArmor.Element("Objs").Descendants("Obj");
+
+                xdocArmor.Descendants("Obj").Where(x => x.Element("ToonName").Value == toonName).Remove();
+                //   xdocArmor.Save(genInventoryFilename);
+                xdocArmor = null;
+
+            }
+            catch (Exception ex) { LogError(ex); }
+
+        }
+
+
+         //This is routine that puts the data of an obj into the armor file xml
+        private void ProcessArmorDataInventory()
+        {
+            for (int n = 0; n < mWaitingForArmorID.Count; n++)
+            {
+                try
+                {
+
+                    if (mWaitingForArmorID[n].HasIdData)
+                    {
+                        WorldObject currentarmorobj = mWaitingForArmorID[n];
+                        mWaitingForArmorID.Remove(mWaitingForArmorID[n]);
+                        //   if ((fn == "armorFilename") && (currentobj.Values(LongValueKey.Imbued) == 0)) { break; }
+                        string armorClassName = currentarmorobj.ObjectClass.ToString();
+                        string armorName = currentarmorobj.Name;
+                        
+                        Int32 armorID = currentarmorobj.Id;
+
+                        Int32 armorIcon = currentarmorobj.Icon;
+
+                        long armorDesc = currentarmorobj.Values(LongValueKey.DescriptionFormat);
+                        long armorMat = currentarmorobj.Values(LongValueKey.Material);
+                        long armorCatType = (int)currentarmorobj.Values(LongValueKey.Category);
+                        long armorAtt = (int)currentarmorobj.Values(LongValueKey.Attuned);
+                        long armorBnd = (int)currentarmorobj.Values(LongValueKey.Bonded);
+                        long armorToonLevel = (int)currentarmorobj.Values(LongValueKey.MinLevelRestrict);
+                        long armorLore = (int)currentarmorobj.Values(LongValueKey.LoreRequirement);
+                        long armorSet = (int)currentarmorobj.Values(LongValueKey.ArmorSet);
+ 
+                        long armorAl = (int)currentarmorobj.Values(LongValueKey.ArmorLevel);
+                        long armorType = (int)currentarmorobj.Values(LongValueKey.Type);
+                        long armorTinks = (int)currentarmorobj.Values(LongValueKey.NumberTimesTinkered);
+                        long armorWork = (int)currentarmorobj.Values(LongValueKey.Workmanship);
+                        long armorBurden = (int)currentarmorobj.Values(LongValueKey.Burden);
+
+                        long armorCovers = currentarmorobj.Values(LongValueKey.Coverage);
+                        long armorEqSlot = currentarmorobj.Values(LongValueKey.EquipableSlots);
+                        long armorWieldValue = (int)currentarmorobj.Values(LongValueKey.WieldReqValue);
+                        long armorSkillLevReq = (int)currentarmorobj.Values(LongValueKey.SkillLevelReq);
+                        double armorSalvWork = currentarmorobj.Values(DoubleValueKey.SalvageWorkmanship);
+                        double armorAcid = currentarmorobj.Values(DoubleValueKey.AcidProt);
+                        double armorLight = currentarmorobj.Values(DoubleValueKey.LightningProt);
+                        double armorFire = currentarmorobj.Values(DoubleValueKey.FireProt);
+                        double armorCold = currentarmorobj.Values(DoubleValueKey.ColdProt);
+                        double armorBludg = currentarmorobj.Values(DoubleValueKey.BludgeonProt);
+                        double armorSlash = currentarmorobj.Values(DoubleValueKey.SlashProt);
+                        double armorPierce = currentarmorobj.Values(DoubleValueKey.PierceProt);
+                        string armorSpellXml = GoGetSpells(currentarmorobj);
+                        long armorRareID = currentarmorobj.Values(LongValueKey.RareId);
+                       long armorModel = currentarmorobj.Values(LongValueKey.Model);
+
+                        xdocArmor.Element("Objs").Add(new XElement("Obj",
+                        new XElement("ArmorName", armorName),
+                        new XElement("ArmorID", armorID),
+                        new XElement("ToonName", toonName),
+                        new XElement("ArmorIcon", armorIcon),
+                        new XElement("ArmorClass", armorClassName),
+                        new XElement("ArmorDesc", armorDesc),
+                        new XElement("ArmorMaterial", armorMat),
+                        new XElement("ArmorAl", armorAl),
+                        new XElement("ArmorSet", armorSet),
+                        new XElement("ArmorCovers", armorCovers),
+                        new XElement("ArmorEqSlot", armorEqSlot),
+                        new XElement("ArmorToonLevel", armorToonLevel),
+                        new XElement("ArmorLoreReq", armorLore),
+                        new XElement("ArmorSkillLevReq", armorSkillLevReq),
+                        new XElement("ArmorWork", armorWork),
+                        new XElement("ArmorTink", armorTinks),
+                        new XElement("ArmorCatType", armorCatType),
+                        new XElement("ArmorType", armorType),
+                        new XElement("ArmorAtt", armorAtt),
+                        new XElement("ArmorBnd", armorBnd),
+                   //     new XElement("ArmorWieldAttr", armorWieldAttrInt),
+                        new XElement("ArmorWieldValue", armorWieldValue),
+                        new XElement("ArmorSalvWork", armorSalvWork),
+                        new XElement("ArmorSpellXml", armorSpellXml),
+                        new XElement("ArmorBurden", armorBurden),
+                        new XElement("ArmorAcid", armorAcid),
+                        new XElement("ArmorLight", armorLight),
+                        new XElement("ArmorFire", armorFire),
+                        new XElement("ArmorCold", armorCold),
+                        new XElement("ArmorBludg", armorBludg),
+                        new XElement("ArmorSlash", armorSlash),
+                        new XElement("ArmorPierce", armorPierce),
+                        new XElement("ArmorRareID", armorRareID)));
+
+
+                        //  xdoc.Save(inventoryFilename);
+
+
+                        currentarmorobj = null;
+                        armorClassName = null;
+                        armorName = null;
+                        armorDesc = 0;
+                        armorID = 0;
+                        armorIcon = 0;
+
+                        armorAl = 0;
+                        armorSet = 0;
+                        armorMat = 0;
+                        armorCovers = 0;
+                        armorToonLevel = 0;
+                        armorLore = 0;
+                        armorSkillLevReq = 0;
+                        armorTinks = 0;
+                        armorWork = 0;
+                        armorCatType = 0;
+                        armorType = 0;
+
+                        armorAtt = 0;
+                        armorBnd = 0;
+                       // objWieldAttrInt = 0;
+                        armorWieldValue = 0;
+                        armorSalvWork = 0;
+                        armorEqSlot = 0;
+                        armorSpellXml = null;
+                        armorRareID = 0;
+                        armorBurden = 0;
+                        armorAcid = 0;
+                        armorLight = 0;
+                        armorFire = 0;
+                        armorCold = 0;
+                        armorBludg = 0;
+                        armorSlash = 0;
+                        armorPierce = 0;
+
+                        armorModel = 0;
+
+                    } // end of if
+
+
+                } // endof try
+
+                catch (Exception ex) { LogError(ex); }
+
+            } // end of for
+
+
+
+        } // end of process data
+
+
+
+
+
+    } // end of partial class
+
+}  // end of namespace
