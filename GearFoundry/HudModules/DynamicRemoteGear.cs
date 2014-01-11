d98298abb458f9e78f72effebb7470a48ef2014a
Kkarnisme <Kkarinisme@aol.com>
2014-01-11 09:56:55 -0500

    Commented out the rendering of armor and inventory for today's runs.

--- GearFoundry/HudModules/DynamicRemoteGear.cs
+++ GearFoundry/HudModules/DynamicRemoteGear.cs
@@ -89,8 +89,8 @@ namespace GearFoundry
 	            if (mGearRemoteSettings.GearPortalRendered) { RenderPortalGearHud(); }
 	            if (mGearRemoteSettings.GearSwitchHRendered) { RenderHorizontalQuickSlots(); }
 	            if (mGearRemoteSettings.GearSwitchVRendered) { RenderVerticalQuickSlots(); }
-	            if (mGearRemoteSettings.GearInventoryRendered) { RenderInventoryHud(); }
-	         	if (mGearRemoteSettings.GearArmorRendered) { RenderArmorHud(); }
+	        //    if (mGearRemoteSettings.GearInventoryRendered) { RenderInventoryHud(); }
+	        // 	if (mGearRemoteSettings.GearArmorRendered) { RenderArmorHud(); }
            }
            catch (Exception ex) { LogError(ex); }
         }
